a264f2de5010592bfd45150f625a9c21cd31c4ad
AlterMannG <98617387+AlterMannG@users.noreply.github.com>
2024-01-15 02:52:24 +0300

    Initial Commit

---
+++ GitHubTest/GitHubTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubTestApp.swift
+//  GitHubTest
+//
+//  Created by Джабраилов Мустафа on 15.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct GitHubTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
